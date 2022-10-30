851636c04cdc1e52ef51e6de05ad9ac951b25ebd
Anubhav Anand <anubhav.ak22@gmail.com>
2022-10-30 12:51:25 +0530

    remove hour if not needed

--- helpers/parseTime.ts
+++ helpers/parseTime.ts
@@ -2,5 +2,5 @@ export const parseNumTime = (num: number) => {
     const hour = Math.floor(num / 3600);
     const min = Math.floor((num - (hour * 3600)) / 60);
     const sec = Math.floor(num - ((hour * 3600) + (min * 60)));
-    return `${hour < 10 ? "0" + hour : hour}:${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`
+    return `${hour !== 0 ? (hour < 10 ? "0" + hour : hour) + ":" : ""}${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`
 }
