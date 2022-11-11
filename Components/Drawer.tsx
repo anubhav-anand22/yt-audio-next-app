cf172a64e8d1baafdcf5d433d4ef9eb0530993a8
anubhav anand <anubhav.ak22@gmail.com>
2022-11-11 17:39:38 +0530

    added history

--- Components/Drawer.tsx
+++ Components/Drawer.tsx
@@ -4,55 +4,87 @@ import { Dispatch, SetStateAction } from "react";
 import { NextRouter, useRouter } from "next/router";
 
 interface DrawerProps {
-    isDrawerShow: boolean;
-    setIsDrawerShow: Dispatch<SetStateAction<boolean>>;
+  isDrawerShow: boolean;
+  setIsDrawerShow: Dispatch<SetStateAction<boolean>>;
 }
 
 interface LinkItemProps {
-    title: string;
-    href: string;
-    route: NextRouter;
+  title: string;
+  href: string;
+  route: NextRouter;
+  onClick?(): void;
+  isMobileOnly?: boolean;
 }
 
 const Drawer = ({ isDrawerShow, setIsDrawerShow }: DrawerProps) => {
-    const route = useRouter();
-    return (
-        <div
-            className={`${style.drawer} ${
-                isDrawerShow ? style.showDrawer : style.hideDrawer
-            }`}
-            onClick={() => setIsDrawerShow(false)}
-        >
-            <nav
-                className={style.drawer_nav}
-                onClick={(e) => e.stopPropagation()}
-            >
-                <div className={style.drawer_nav_search}>
-                    <input type="url" placeholder="Url" />
-                    <button>Go</button>
-                </div>
-                <ul className={style.drawer_nav_ul}>
-                    <LinkItem href="/" title="Home" route={route} />
-                    <LinkItem href="/setting" title="Setting" route={route} />
-                    <LinkItem href="/about" title="About me" route={route} />
-                </ul>
-            </nav>
+  const route = useRouter();
+
+  const onItemClick = () => {
+    setIsDrawerShow(false);
+  };
+  return (
+    <div
+      className={`${style.drawer} ${
+        isDrawerShow ? style.showDrawer : style.hideDrawer
+      }`}
+      onClick={() => setIsDrawerShow(false)}
+    >
+      <nav className={style.drawer_nav} onClick={(e) => e.stopPropagation()}>
+        <div className={style.drawer_nav_search}>
+          <input type="url" placeholder="Url" />
+          <button>Go</button>
         </div>
-    );
+        <ul className={style.drawer_nav_ul}>
+          <LinkItem
+            href="/"
+            title="Home"
+            route={route}
+            onClick={onItemClick}
+            isMobileOnly
+          />
+          <LinkItem
+            href="/setting"
+            title="Setting"
+            route={route}
+            onClick={onItemClick}
+            isMobileOnly
+          />
+          <LinkItem
+            href="/about"
+            title="About me"
+            route={route}
+            onClick={onItemClick}
+          />
+          <LinkItem
+            href="/history"
+            title="History"
+            route={route}
+            onClick={onItemClick}
+          />
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
-const LinkItem = ({ href, title, route }: LinkItemProps) => {
-    return (
-        <Link href={href}>
-            <li
-                className={`${
-                    route.pathname === href ? style.activePathLi : ""
-                }`}
-            >
-                {title}
-            </li>
-        </Link>
-    );
+const LinkItem = ({
+  href,
+  title,
+  route,
+  onClick = () => {},
+  isMobileOnly,
+}: LinkItemProps) => {
+  return (
+    <Link
+      className={`${isMobileOnly ? style.mobileOnly : ""}`}
+      onClick={onClick}
+      href={href}
+    >
+      <li className={`${route.pathname === href ? style.activePathLi : ""}`}>
+        {title}
+      </li>
+    </Link>
+  );
 };
 
 export default Drawer;
