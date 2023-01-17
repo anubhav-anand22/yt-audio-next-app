022f749600028bbe83a7e45a0268757a3041f518
anubhav anand <anubhav.ak22@gmail.com>
2023-01-17 19:11:49 +0530

    favVideo DB

--- db/dataDb.ts
+++ db/dataDb.ts
@@ -1,20 +1,22 @@
-import Dexie, { Table } from 'dexie';
+import Dexie, { Table } from "dexie";
 
 export class MySubClassedDexie extends Dexie {
-  videoItem!: Table<VieoItemResDataDbObj>; 
+  videoItem!: Table<VieoItemResDataDbObj>;
   videoDetails!: Table<VideoDetailsDataDbObj>;
   historyList!: Table<HistoryDbListType>;
   historyVideo!: Table<HistoryDbVideoType>;
   historySearch!: Table<HistoryDbSearchType>;
+  favVideo!: Table<FavDbVideoType>;
 
   constructor() {
-    super('myDatabase');
-    this.version(5).stores({
-        videoItem: 'videoId',
-        videoDetails: "listId",
-        historyList: 'id, title',
-        historyVideo: "id, title, owner",
-        historySearch: "query"
+    super("myDatabase");
+    this.version(6).stores({
+      videoItem: "videoId",
+      videoDetails: "listId",
+      historyList: "id, title",
+      historyVideo: "id, title, owner",
+      historySearch: "query",
+      favVideo: "id",
     });
   }
 }
