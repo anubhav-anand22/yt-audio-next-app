304c5844dfe3587e1ce5eaca7de766003898a3cf
anubhav anand <anubhav.ak22@gmail.com>
2023-01-18 13:15:54 +0530

    remove volume change rate

--- pages/setting.tsx
+++ pages/setting.tsx
@@ -1,9 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { setIsDark, toggleIsDark } from "../store/settingSlice";
-import style from '../styles/Pages/setting.module.css'
+import style from "../styles/Pages/setting.module.css";
 
 export default function Setting() {
-
   const { isDark } = useSelector((state: GStateType) => state.setting);
   const dispatch = useDispatch();
 
@@ -16,17 +15,17 @@ export default function Setting() {
 
   return (
     <div className={style.settingPage}>
-      <div className={style.itemBool}>
+      <div className={style.itemBtn}>
         <p>Theme:</p>
         <button onClick={onThemeChange}>{isDark ? "Dark" : "Light"}</button>
       </div>
-      <div className={style.itemRange}>
-        <p>Volume change rate:</p>
-        <div className={style.itemRangeRangeCont}>
-          <p>1</p>
-          <input max={20} min={1} type="range" />
-          <p>20</p>
-        </div>
+      <div className={style.itemBtn}>
+        <p>Export data:</p>
+        <button>Export</button>
+      </div>
+      <div className={style.itemBtn}>
+        <p>Import data:</p>
+        <button>Import</button>
       </div>
     </div>
   );
