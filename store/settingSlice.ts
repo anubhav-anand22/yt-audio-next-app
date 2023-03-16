ffcba99a367fb2a76d5502c56c78cf4a0badf189
anubhav anand <anubhav.ak22@gmail.com>
2023-03-16 23:28:45 +0530

    setting global state

--- store/settingSlice.ts
+++ store/settingSlice.ts
@@ -1,26 +1,32 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const defaultSetting = {
-    isDark: false
-}
+  isDark: false,
+  cacheAudio: true,
+  cacheAudioNumber: 100,
+  id: 1,
+};
 
 export const settingSlice = createSlice({
-    name: "SETTING",
-    initialState: defaultSetting,
-    reducers: {
-        setIsDark(state, action: PayloadAction<boolean>) {
-            const newState = state;
-            state.isDark = action.payload
-            return newState
-        },
-        toggleIsDark(state) {
-            const newState = state;
-            state.isDark = !state.isDark;
-            return newState;
-        }
+  name: "SETTING",
+  initialState: defaultSetting,
+  reducers: {
+    setIsDark(state, action: PayloadAction<boolean>) {
+      const newState = state;
+      state.isDark = action.payload;
+      return newState;
     },
+    toggleIsDark(state) {
+      const newState = state;
+      state.isDark = !state.isDark;
+      return newState;
+    },
+    initSetting(state, action: PayloadAction<SettingVarsU>) {
+      const newState = { ...defaultSetting, ...state, ...action.payload };
+      return newState;
+    },
+  },
 });
 
-export const { setIsDark, toggleIsDark } =
-    settingSlice.actions;
+export const { setIsDark, toggleIsDark, initSetting } = settingSlice.actions;
 export default settingSlice.reducer;
